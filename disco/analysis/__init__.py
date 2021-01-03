2daad0cde4f1320d7865aca1fbe04480f814f454
jgu2 <jianli.gu@nrel.gov>
2021-01-03 00:40:08 -0700

    Test upgrade cost analysis

--- disco/analysis/__init__.py
+++ disco/analysis/__init__.py
@@ -4,3 +4,8 @@ from .option import *
 from .snapshot_impact_analysis import *
 from .time_series_impact_analysis import *
 from .upgrade_cost_analysis import *
+
+GENERIC_COST_DATABASE = os.path.join(
+    os.path.dirname(__file__),
+    "generic_cost_database_v1.xlsx"
+)
