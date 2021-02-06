f3e5175675bb3ac40a665d90ccec7ec7dcfc8273
KwamiS <senam.sedzro@gmail.com>
2021-02-05 18:18:45 -0700

    postprocess

---
+++ disco/analysis/postprocess_time_series.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 07:46:25 2021
+
+@author: senam
+"""
+import os
+import json
+import pandas as pd
+import numpy as np
+
+from PyDSS.pydss_project import PyDssProject
+from disco.extensions.pydss_simulation.pydss_configuration import PyDssConfiguration
+from disco.distribution.deployment_parameters import DeploymentParameters
+
+
+        
+def combine_metrics(project_path):
+    summary_dict = dict()
+    
+    
+    #if os.path.exists(project_path):
+    pydss_project = PyDssProject.load_project(project_path)
+    
+    voltage_metrics = json.loads(
+        pydss_project.fs_interface.read_file(
+            os.path.join("Reports", "voltage_metrics.json")
+        )
+    )
+     
+    thermal_metrics = json.loads(
+        pydss_project.fs_interface.read_file(
+            os.path.join("Reports", "thermal_metrics.json")
+        )
+    )
+    print()
+    print(thermal_metrics)   
+    print()
+    summary_dict.update(voltage_metrics['summary']) 
+    summary_dict.update(thermal_metrics['summary'])
+    return summary_dict
+
+def get_absolute_changes(df, property_name, base_case='base_case'):
+    to_exclude = [
+        'total_num_time_points',
+        'total_simulation_duration', 
+        'num_nodes_always_inside_ansi_a' 
+    ]
+    if property_name not in to_exclude:
+        df[f"absolute_change_in_{property_name}"] = (
+            df.property_name - df.loc[base_case, property_name]
+        )
+    return df
+
+def aggregate_deployments(job_outputs_path):
+    
+    config_file = os.path.join(os.path.dirname(job_outputs_path), "config.json")
+    config = PyDssConfiguration.deserialize(config_file)
+    all_summaries_dict = dict()
+    
+    
+        
+    base_case = [job for job in config.iter_jobs() if isinstance(job, DeploymentParameters) and job.model.is_base_case][0]
+    print(base_case)
+    for job in config.iter_jobs():
+        project_path = os.path.join(
+            job_outputs_path, job.name
+        )
+        
+        all_summaries_dict[job.name] = combine_metrics(project_path)
+        
+    print(all_summaries_dict)    
+    summary_df = pd.DataFrame.from_dict(all_summaries_dict, 'index')
+    if not summary_df.empty:
+        for property_name in summary_df.columns:
+            summary_df = get_absolute_changes(summary_df, property_name, base_case.name)
+        summary_df = assess_deployments(summary_df)
+            
+    return summary_df
+
+def assess_deployments(df):
+    key = 'absolute_change_in'
+    change_cols = [(c, c.split(key)[1]) for c in df.columns if key in c]
+    for cols in change_cols:
+        change_col = cols[0]
+        flag_col = f"pass_{cols[1]}"
+        df.loc[:, flag_col] = df[change_col]<=0
+    pass_flags = [c for c in df.columns if c.startswith('pass')]
+    df['pass_flag'] = df[pass_flags[0]]
+    for col in pass_flags[1:]:
+        df['pass_flag'] = np.logical_and(df['pass_flag'], df[col])
+        
+    return df
+    
+    
+    
+
---
+++ disco/cli/compute_time_series_impact_analysis.py
@@ -0,0 +1,33 @@
+
+import logging
+
+import click
+import os
+
+from jade.loggers import setup_logging
+from jade.utils.utils import get_cli_string
+from disco.analysis.postprocess_time_series import aggregate_deployments
+#from jade.common import CONFIG_FILE
+
+
+@click.command()
+@click.argument("feeder")
+@click.option(
+    "-o", "--jade-runtime-output",
+    required=True,
+    help="jade runtime output directory",
+)
+@click.option(
+    "--verbose",
+    is_flag=True,
+    default=False,
+    help="Enable debug logging",
+)
+def compute_time_series_impact_analysis(feeder, jade_runtime_output, verbose=False):
+    """Run post-process computations for time series impact analysis."""
+    level = logging.DEBUG if verbose else logging.INFO
+    logger = setup_logging(__name__, None, console_level=level)
+    job_outputs = os.path.join(jade_runtime_output, "job-outputs")
+    df = aggregate_deployments(job_outputs)
+    df.to_csv(os.path.join(jade_runtime_output, 'impact_summary.csv'))
+    print(get_cli_string())
