6a412383a95d997ab9bb69c44a92056ba8e36702
Daniel Thom <daniel.thom@nrel.gov>
2021-03-21 18:10:39 -0600

    Fix parsing of substations for transform-model

--- disco/sources/source_tree_1/source_tree_1_model.py
+++ disco/sources/source_tree_1/source_tree_1_model.py
@@ -375,8 +375,6 @@ class SourceTree1Model(BaseOpenDssModel):
 
         if substations == ("all",):
             substations = inputs.list_substations()
-        if feeders == ("all",):
-            feeders = inputs.list_feeders()
         if placements == ("all",):
             placements = inputs.list_placements()
         elif not isinstance(placements[0], float):
@@ -384,112 +382,113 @@ class SourceTree1Model(BaseOpenDssModel):
 
         config = []
         base_cases = set()
-        for substation, feeder, placement in itertools.product(
-            substations, feeders, placements
-        ):
-            base_case_name = SourceTree1Model.make_base_case_name(substation, feeder)
-            if base_case_name not in base_cases and issubclass(simulation_model, ImpactAnalysisBaseModel):
-                data = {
-                    "path": input_path,
-                    "substation": substation,
-                    "feeder": feeder,
-                    "master": master_file,
-                    "placement": None,
-                    "deployment": None,
-                    "penetration_level": None,
-                    "deployment_file": None,
-                    "loadshape_directory": None,
-                    "opendss_directory": inputs.get_opendss_directory(
-                        substation, feeder
-                    ),
-                    "pv_locations": [],
-                    "pydss_controllers": None,
-                    "is_base_case": True,
-                }
-                model = cls(data)
-                path = os.path.join(output_path, substation, feeder)
-                out_deployment = model.create_base_case(base_case_name, path)
-                item = {
-                    "deployment": out_deployment,
-                    "simulation": simulation_params,
-                    "name": base_case_name,
-                    "model_type": simulation_model.__name__,
-                    "job_order": 0,
-                    "is_base_case": True,
-                }
-                config.append(simulation_model.validate(item).dict())
-                base_cases.add(base_case_name)
-            key = inputs.create_key(substation, feeder, placement)
-            if deployments == ("all",):
-                _deployments = inputs.list_deployments(key)
-            else:
-                _deployments = [int(x) for x in deployments]
-            for deployment in _deployments:
-                if penetration_levels == ("all",):
-                    levels = inputs.list_penetration_levels(key, deployment)
-                else:
-                    levels = [int(x) for x in penetration_levels]
-                for level in levels:
-                    deployment_file = inputs.get_deployment_file(key, deployment, level)
-                    pv_configs = inputs.list_pv_configs(
-                        substation, feeder, placement, deployment
-                    )
-                    # Validation of a PyDSSControllerModel is currently slow.
-                    # This is a workaround.
-                    # TODO DT
-                    # update: may be fixed. test on large dataset
-                    pydss_controllers = set()
-                    pv_profiles = {}
-                    pydss_controller = None
-                    for pv_config in pv_configs:
-                        # TODO DT: the overall model needs to support a mapping
-                        # instead of a single controller
-                        tmp = pv_config["pydss_controller"]
-                        ctrl = (tmp["controller_type"], tmp["name"])
-                        if ctrl[1] != "pf1" and ctrl not in pydss_controllers:
-                            pydss_controller = PyDSSControllerModel.validate(
-                                pv_config["pydss_controller"]
-                            )
-                            pydss_controllers.add(ctrl)
-                        pv_profiles[pv_config["name"]] = pv_config["pv_profile"]
-                    if len(pydss_controllers) > 1:
-                        raise Exception(
-                            f"only 1 pydss controller is currently supported: {pydss_controllers}"
-                        )
+        for substation, placement in itertools.product(substations, placements):
+            for feeder in inputs.list_feeders(substation):
+                if feeders != ("all",) and feeder not in feeders:
+                    continue
+                base_case_name = SourceTree1Model.make_base_case_name(substation, feeder)
+                if base_case_name not in base_cases and issubclass(simulation_model, ImpactAnalysisBaseModel):
                     data = {
                         "path": input_path,
                         "substation": substation,
                         "feeder": feeder,
                         "master": master_file,
-                        "placement": placement.value,
-                        "deployment": deployment,
-                        "penetration_level": level,
-                        "deployment_file": deployment_file,
+                        "placement": None,
+                        "deployment": None,
+                        "penetration_level": None,
+                        "deployment_file": None,
                         "loadshape_directory": None,
                         "opendss_directory": inputs.get_opendss_directory(
                             substation, feeder
                         ),
-                        "pv_locations": [deployment_file],
-                        "pydss_controllers": pydss_controller,
-                        "is_base_case": False,
+                        "pv_locations": [],
+                        "pydss_controllers": None,
+                        "is_base_case": True,
                     }
-                    # TODO DT: add pv_profiles to models?
-                    # TODO DT: change 'deployment' to 'sample', per Kwami
                     model = cls(data)
                     path = os.path.join(output_path, substation, feeder)
-                    out_deployment = model.create_deployment(
-                        model.name, path, pv_profile=pv_profiles
-                    )
+                    out_deployment = model.create_base_case(base_case_name, path)
                     item = {
                         "deployment": out_deployment,
                         "simulation": simulation_params,
-                        "name": model.name,
+                        "name": base_case_name,
                         "model_type": simulation_model.__name__,
-                        "job_order": level
+                        "job_order": 0,
+                        "is_base_case": True,
                     }
-                    if issubclass(simulation_model, ImpactAnalysisBaseModel):
-                        item["base_case"] = base_case_name
                     config.append(simulation_model.validate(item).dict())
+                    base_cases.add(base_case_name)
+                key = inputs.create_key(substation, feeder, placement)
+                if deployments == ("all",):
+                    _deployments = inputs.list_deployments(key)
+                else:
+                    _deployments = [int(x) for x in deployments]
+                for deployment in _deployments:
+                    if penetration_levels == ("all",):
+                        levels = inputs.list_penetration_levels(key, deployment)
+                    else:
+                        levels = [int(x) for x in penetration_levels]
+                    for level in levels:
+                        deployment_file = inputs.get_deployment_file(key, deployment, level)
+                        pv_configs = inputs.list_pv_configs(
+                            substation, feeder, placement, deployment
+                        )
+                        # Validation of a PyDSSControllerModel is currently slow.
+                        # This is a workaround.
+                        # TODO DT
+                        # update: may be fixed. test on large dataset
+                        pydss_controllers = set()
+                        pv_profiles = {}
+                        pydss_controller = None
+                        for pv_config in pv_configs:
+                            # TODO DT: the overall model needs to support a mapping
+                            # instead of a single controller
+                            tmp = pv_config["pydss_controller"]
+                            ctrl = (tmp["controller_type"], tmp["name"])
+                            if ctrl[1] != "pf1" and ctrl not in pydss_controllers:
+                                pydss_controller = PyDSSControllerModel.validate(
+                                    pv_config["pydss_controller"]
+                                )
+                                pydss_controllers.add(ctrl)
+                            pv_profiles[pv_config["name"]] = pv_config["pv_profile"]
+                        if len(pydss_controllers) > 1:
+                            raise Exception(
+                                f"only 1 pydss controller is currently supported: {pydss_controllers}"
+                            )
+                        data = {
+                            "path": input_path,
+                            "substation": substation,
+                            "feeder": feeder,
+                            "master": master_file,
+                            "placement": placement.value,
+                            "deployment": deployment,
+                            "penetration_level": level,
+                            "deployment_file": deployment_file,
+                            "loadshape_directory": None,
+                            "opendss_directory": inputs.get_opendss_directory(
+                                substation, feeder
+                            ),
+                            "pv_locations": [deployment_file],
+                            "pydss_controllers": pydss_controller,
+                            "is_base_case": False,
+                        }
+                        # TODO DT: add pv_profiles to models?
+                        # TODO DT: change 'deployment' to 'sample', per Kwami
+                        model = cls(data)
+                        path = os.path.join(output_path, substation, feeder)
+                        out_deployment = model.create_deployment(
+                            model.name, path, pv_profile=pv_profiles
+                        )
+                        item = {
+                            "deployment": out_deployment,
+                            "simulation": simulation_params,
+                            "name": model.name,
+                            "model_type": simulation_model.__name__,
+                            "job_order": level
+                        }
+                        if issubclass(simulation_model, ImpactAnalysisBaseModel):
+                            item["base_case"] = base_case_name
+                        config.append(simulation_model.validate(item).dict())
 
         os.makedirs(output_path, exist_ok=True)
         filename = os.path.join(output_path, SOURCE_CONFIGURATION_FILENAME)
