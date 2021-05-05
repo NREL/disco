5d06d0a4c2d4b02945436fceac140c3832319263
Daniel Thom <daniel.thom@nrel.gov>
2021-05-05 13:10:53 -0600

    Make summary tables from hosting capacity batches

--- disco/cli/disco_internal.py
+++ disco/cli/disco_internal.py
@@ -8,6 +8,7 @@ from disco.cli.compute_snapshot_hosting_capacity import compute_snapshot_hosting
 from disco.cli.compute_snapshot_impact_analysis import compute_snapshot_impact_analysis
 from disco.cli.compute_time_series_hosting_capacity import compute_time_series_hosting_capacity
 from disco.cli.compute_time_series_impact_analysis import compute_time_series_impact_analysis
+from disco.cli.make_hosting_capacity_summary_tables import make_summary_tables
 
 
 logger = logging.getLogger(__name__)
@@ -22,3 +23,4 @@ cli.add_command(compute_snapshot_hosting_capacity)
 cli.add_command(compute_snapshot_impact_analysis)
 cli.add_command(compute_time_series_hosting_capacity)
 cli.add_command(compute_time_series_impact_analysis)
+cli.add_command(make_summary_tables)
