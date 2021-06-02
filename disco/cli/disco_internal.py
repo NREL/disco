69ee4296cd124658ae9bfa624139a628a3acf53b
Daniel Thom <daniel.thom@nrel.gov>
2021-06-02 08:47:07 -0600

    Rename file to more appropriate name

--- disco/cli/disco_internal.py
+++ disco/cli/disco_internal.py
@@ -8,7 +8,7 @@ from disco.cli.compute_snapshot_hosting_capacity import compute_snapshot_hosting
 from disco.cli.compute_snapshot_impact_analysis import compute_snapshot_impact_analysis
 from disco.cli.compute_time_series_hosting_capacity import compute_time_series_hosting_capacity
 from disco.cli.compute_time_series_impact_analysis import compute_time_series_impact_analysis
-from disco.cli.make_hosting_capacity_summary_tables import make_summary_tables
+from disco.cli.make_summary_tables import make_summary_tables
 
 
 logger = logging.getLogger(__name__)
