dad1d2ce47f12f7ae487068bdeb30c1e3d33f9a2
jgu2 <jianli.gu@nrel.gov>
2021-06-11 03:13:44 -0600

    Make clean query UI & modules

--- disco/postprocess/toolbox/query_tool.py
+++ disco/postprocess/toolbox/query_tool.py
@@ -1,3 +1,118 @@
+import os
+from pprint import pprint
+from types import SimpleNamespace
+
+import pandas as pd
+import ipywidgets as widgets
+from IPython.display import display
+
+from disco.postprocess.hosting_capacity import get_hosting_capacity
+
+# Layouts
+field_layout = widgets.Layout(width="50%")
+comp_layout = widgets.Layout(width="10%")
+value_layout = widgets.Layout(width="40%")
+textarea_layout = widgets.Layout(width="99.5%", height="200px")
+
+# Options
+comparison_options = ["==", "<", "<=", ">", ">="]
+bool_options = [False, True]
+scenario_options = ["scenario", "pf1", "control_mode"]
+node_type_options = ["primaries", "secondaries"]
+placement_options = ["close", "far", "random"]
+
+# Namespaces
+inputs = SimpleNamespace(
+    feeder_head=None,
+    feeder_losses=None,
+    metadata=None,
+    thermal_metrics=None,
+    voltage_metrics=None
+)
+
+queryset = SimpleNamespace(
+    feeder_head=None,
+    feeder_losses=None,
+    metadata=None,
+    thermal_metrics=None,
+    voltage_metrics=None
+)
+
+hc = SimpleNamespace(
+    summary=None,
+    overall=None
+)
+
+# Load Data
+load_tab = widgets.Tab(indent=False)
+load_tab.set_title(0, "Load Tables")
+
+input_path = widgets.Text(placeholder="result directory of summary tables", indent=False, layout=widgets.Layout(width="99.5%"))
+feeder_head_table_checkbox = widgets.Checkbox(description="Feeder Head Table", value=False, indent=False)
+feeder_losses_table_checkbox = widgets.Checkbox(description="Feeder Losses Table", value=False, indent=False)
+metadata_table_checkbox = widgets.Checkbox(description="Metadata Table", value=False, indent=False)
+thermal_metrics_table_checkbox = widgets.Checkbox(description="Thermal Metrics Table", value=False, indent=False)
+voltage_metrics_table_checkbox = widgets.Checkbox(description="Voltage Metrics Table", value=False, indent=False)
+load_box = widgets.VBox([
+    input_path,
+    feeder_head_table_checkbox,
+    feeder_losses_table_checkbox,
+    metadata_table_checkbox,
+    thermal_metrics_table_checkbox,
+    voltage_metrics_table_checkbox
+])
+
+load_tab.children = [load_box]
+load_button = widgets.Button(description="Load Metrics", indent=False)
+load_output = widgets.Output(indent=False)
+
+def load_tables(arg):
+    """Load metrics tables into memory"""
+    load_output.clear_output()
+    with load_output:
+        if not input_path.value:
+            print("Please provide the output directory of metrics tables.")
+            return
+
+        if not os.path.exists(input_path.value):
+            print(f"Output directory does not exist! '{input_path.value}'")
+            return
+        
+        selected = False
+        if feeder_head_table_checkbox.value:
+            selected = True
+            feeder_head_table = os.path.join(input_path.value, "feeder_head_table.csv")
+            inputs.feeder_head = pd.read_csv(feeder_head_table)
+        
+        if feeder_losses_table_checkbox.value:
+            selected = True
+            feeder_losses_table = os.path.join(input_path.value, "feeder_losses_table.csv")
+            inputs.feeder_losses = pd.read_csv(feeder_losses_table)
+        
+        if metadata_table_checkbox.value:
+            selected = True
+            metadata_table = os.path.join(input_path.value, "metadata_table.csv")
+            inputs.metadata = pd.read_csv(metadata_table)
+        
+        if thermal_metrics_table_checkbox.value:
+            selected = True
+            thermal_metrics_table = os.path.join(input_path.value, "thermal_metrics_table.csv")
+            inputs.thermal_metrics = pd.read_csv(thermal_metrics_table)
+        
+        if voltage_metrics_table_checkbox.value:
+            selected = True
+            voltage_metrics_table = os.path.join(input_path.value, "voltage_metrics_table.csv")
+            inputs.voltage_metrics = pd.read_csv(voltage_metrics_table)
+
+        if selected:
+            print("Selected tables loaded, check 'inputs' namespace.")
+        else:
+            print("No table selected, please select first.")
+
+display(load_tab)
+display(load_button, load_output)
+load_button.on_click(load_tables)
+
 
 # Tab
 tables_tab = widgets.Tab(indent=False)
@@ -263,7 +378,7 @@ def query_tables(arg):
     if feeder_head_table_checkbox.value and inputs.feeder_head is not None:
         feeder_head_query_string = build_query_string(feeder_head_hboxes, feeder_head_columns)
         if feeder_head_query_string:
-            outputs.feeder_head = inputs.feeder_head.query(feeder_head_query_string)
+            queryset.feeder_head = inputs.feeder_head.query(feeder_head_query_string)
 
     if feeder_losses_table_checkbox.value and inputs.feeder_losses is not None:
         feeder_losses_query_string = build_query_string(feeder_losses_hboxes, feeder_losses_columns)
@@ -278,23 +393,80 @@ def query_tables(arg):
     if thermal_metrics_table_checkbox.value and inputs.thermal_metrics is not None:
         thermal_metrics_query_string = build_query_string(thermal_metrics_hboxes, thermal_metrics_columns)
         if thermal_metrics_query_string:
-            outputs.thermal_metrics = inputs.thermal_metrics.query(thermal_metrics_query_string)
+            queryset.thermal_metrics = inputs.thermal_metrics.query(thermal_metrics_query_string)
 
     if voltage_metrics_table_checkbox.value and inputs.voltage_metrics is not None:
         voltage_metrics_query_string = build_query_string(voltage_metrics_hboxes, voltage_metrics_columns)
         if voltage_metrics_query_string:
-            outputs.voltage_metrics = inputs.voltage_metrics.query(voltage_metrics_query_string)
+            queryset.voltage_metrics = inputs.voltage_metrics.query(voltage_metrics_query_string)
     
     query_output.clear_output()
     with query_output:
-        print("Query done! Check the 'outputs' variable.")
+        print("Query done! Check the 'queryset' namespace.")
 
+
+def compute_thermal_hc(hc_summary):
+    metric_df = queryset.thermal_metrics
+    if metric_df is None:
+        return
+    metric_df = metric_df.mask(metric_df.eq("None")).dropna()
+    metric_df.penetration_level = metric_df.penetration_level.astype("float")
+
+    query_phrase = thermal_metrics_textarea.value
+    if query_phrase:
+        hc_summary = get_hosting_capacity(
+            inputs.metadata, metric_df, query_phrase, "thermal", hc_summary
+        )
+
+
+def compute_voltage_hc(hc_summary):
+    metric_df = queryset.voltage_metrics
+    if metric_df is None:
+        return
+    metric_df = metric_df.mask(metric_df.eq("None")).dropna()
+    metric_df.penetration_level = metric_df.penetration_level.astype("float")
+
+    query_phrase = voltage_metrics_textarea.value
+    if query_phrase:
+        hc_summary = get_hosting_capacity(
+            inputs.metadata, metric_df, query_phrase, "voltage", hc_summary
+        )
+
+
+def compute_hc(arg):
+    query_output.clear_output()
+    if not metadata_table_checkbox.value:
+        with query_output:
+            print("Metadata table has not been loaded, please load first.")
+        return
+    
+    hc_summary = {}
+    hc_overall = {}
+    compute_thermal_hc(hc_summary)
+    compute_voltage_hc(hc_summary)
+    
+    for feeder, data in hc_summary.items():
+        hc_overall[feeder] = {}
+        df = pd.DataFrame.from_dict(data, "index")
+        for column in df.columns:
+            hc_overall[feeder][column] = min(df[column])
+
+    hc.summary = hc_summary
+    hc.overall = hc_overall
+    
+    with query_output:
+        print("Compute done! Check the 'hc' namespace.")
+
+
+# Query UI
 tables_tab.children = [feeder_head_vbox, feeder_losses_vbox, metadata_vbox, thermal_metrics_vbox, voltage_metrics_vbox]
 display(tables_tab)
 
 build_button = widgets.Button(description="Build Query", indent=False)
 query_button = widgets.Button(description="Run Query", indent=False)
+compute_button = widgets.Button(description="Compute HC", indent=False)
 build_button.on_click(build_queries)
 query_button.on_click(query_tables)
-display(widgets.HBox([build_button, query_button]))
+compute_button.on_click(compute_hc)
+display(widgets.HBox([build_button, query_button, compute_button]))
 display(query_output)
