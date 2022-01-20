0d4326cd17c17f8768e4f601882e0f5901268eb9
jgu2 <jianli.gu@nrel.gov>
2022-01-19 22:34:46 -0700

    Init commit new upgrades

---
+++ disco/extensions/upgrade_simulation/upgrade_parameters.py
@@ -0,0 +1,13 @@
+
+from disco.distribution.deployment_parameters import DeploymentParameters
+
+
+class UpgradeParameters(DeploymentParameters):
+    """Represents deployment parameters for upgrade simulation/analysis
+
+    Parameters
+    ----------
+    DeploymentParameters : object
+        Deployment Job
+    """
+
