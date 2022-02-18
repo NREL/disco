82feac0330722621abe6cea7d8fc30468ea94bbb
wwang2 <wwang2@nrel.gov>
2022-02-17 17:18:48 -0700

    add metrics plot

--- notebooks/plots.py
+++ notebooks/plots.py
@@ -28,15 +28,13 @@ df2 = pd.read_sql_query("PRAGMA table_info(hosting_capacity)", conn)
 df_hc = pd.read_sql_query("SELECT * from hosting_capacity Where time_point=='max_pv_load_ratio' and scenario=='pf1' and hc_type=='overall'", conn)
 
 
-
+# choose number of feeders 30
 df_hc_test = df_hc.iloc[0:30]
 fig, ax = plt.subplots(figsize=(8,8))
 y_pos = df_hc_test['feeder']
 ax.barh(y_pos,df_hc_test['min_hc_pct'],label="no violation",color='limegreen')
 ax.barh(y_pos,df_hc_test['max_hc_pct']-df_hc_test['min_hc_pct'],left=df_hc_test['min_hc_pct'],label="some violation",color='gold')
 ax.barh(y_pos,200-df_hc_test['max_hc_pct'],left=df_hc_test['max_hc_pct'],label="violation",color='tomato')
-
-
 ax.set_title("HCA heatmap: pf1")
 ax.set_xlabel("Penetration level (%)")
 ax.legend(ncol=3)
@@ -45,3 +43,35 @@ plt.savefig("hca.png")
 # color match: https://www.xcelenergy.com/hosting_capacity_map 
 
 
+# select a feeder name
+
+feeder_example = df_hc_test['feeder'][1]
+
+df3 = pd.read_sql_query(f"SELECT * from voltage_metrics Where feeder=='{feeder_example}'", conn)
+
+fig, ax = plt.subplots(figsize=(8,8))
+ax.scatter(df3[df3['node_type']=='primaries']['penetration_level'],df3[df3['node_type']=='primaries']["max_voltage"],facecolors='none',edgecolors='C0',label="primary")
+ax.scatter(df3[df3['node_type']=='secondaries']['penetration_level'],df3[df3['node_type']=='secondaries']["max_voltage"],facecolors='none',edgecolors='C1',label="secondary")
+ax.legend()
+ax.set_title(feeder_example)
+ax.set_xlabel("Penetration level")
+ax.set_ylabel("max_voltage (pu)")
+fig.savefig("max_voltage_pri_sec.png")
+
+
+fig, ax = plt.subplots(figsize=(8,8))
+
+ax.scatter(df3[df3['scenario']=='pf1']['penetration_level'],df3[df3['scenario']=='pf1']["max_voltage"],facecolors='none',edgecolors='C0',label="base_case:pf1")
+ax.scatter(df3[df3['scenario']=='control_mode']['penetration_level'],df3[df3['scenario']=='control_mode']["max_voltage"],facecolors='none',edgecolors='C1',label="control_mode:volt-var")
+ax.legend()
+ax.set_title(feeder_example)
+ax.set_xlabel("Penetration level")
+ax.set_ylabel("max_voltage (pu)")
+fig.savefig("max_voltage_base_voltvar.png")
+
+
+
+
+
+
+print('End')
