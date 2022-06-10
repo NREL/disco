decbf464da40dd2b7d71b581b15c7c2dc996583a
Duwadi, Kapil <Kapil.Duwadi@nrel.gov>
2022-06-10 16:00:46 -0600

    Updated doc string for hosting_capacity_by_timestep

--- disco/cli/pydss_hosting_capacity.py
+++ disco/cli/pydss_hosting_capacity.py
@@ -34,7 +34,11 @@ def hosting_capacity_by_timestep(
     jade_output_dir, output_json, overvoltage_threshold, thermal_threshold
 ):
 
-    """Compute hosting capacity each time step for all feeders"""
+    """Parse all the jobs in JADE output directory and compute hosting capacity for each
+    feeders by scenario and by time step taking pydss results. This script assumes that you have
+    already ran time series simulation for all your scenarios and exported bus voltage and thermal
+    overloading metrics. Hosting capacity is computed by analyzing maximum voltage and/or maximum thermal
+    overloading across all samples and placements for given substation scenario combination."""
     compute_hosting_capacity_for_pydss(
         jade_output_dir, output_json, overvoltage_threshold, thermal_threshold
     )
