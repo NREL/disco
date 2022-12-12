1a48051ba6f9a154426171cdd0c4e7434cf025d2
Daniel Thom <daniel.thom@nrel.gov>
2022-12-12 10:56:08 -0700

    Code cleanup

--- disco/preprocess/select_timepoints2.py
+++ disco/preprocess/select_timepoints2.py
@@ -5,32 +5,38 @@ Created on Fri Dec  2 12:13:24 2022
 @author: ksedzro
 """
 
-
+import logging
 import os
+import time
+
 import numpy as np
 import pandas as pd
-import time
 import opendssdirect as dss
 
 
+logger = logging.getLogger(__name__)
+
+
 def load_feeder(path_to_master):
-    if os.path.exists(path_to_master):
-        dpath, fname = os.path.split(path_to_master)
-        newfname = fname.split(".")[0]
-        new_master = os.path.join(dpath, f"{newfname}_new.dss")
-        with open(path_to_master, "r") as mr:
-            slines = mr.readlines()
-        found_solve = False
-        lines = []
-        for line in slines:
-            if line.lower().lstrip().startswith("solve"):
-                line = "Solve\n"
-                found_solve = True
-            lines.append(line)
-        if not found_solve:
-            lines.append("Solve\n")
-        with open(new_master, "w") as mw:
-            mw.writelines(lines)
+    if not os.path.exists(path_to_master):
+        raise FileNotFoundError(path_to_master)
+
+    dpath, fname = os.path.split(path_to_master)
+    newfname = fname.split(".")[0]
+    new_master = os.path.join(dpath, f"{newfname}_new.dss")
+    with open(path_to_master, "r") as mr:
+        slines = mr.readlines()
+    found_solve = False
+    lines = []
+    for line in slines:
+        if line.lower().lstrip().startswith("solve"):
+            line = "Solve\n"
+            found_solve = True
+        lines.append(line)
+    if not found_solve:
+        lines.append("Solve\n")
+    with open(new_master, "w") as mw:
+        mw.writelines(lines)
 
     dss.Text.Command(f"redirect {new_master}")
 
@@ -120,11 +126,14 @@ def get_param_values(param_class, bus_data, category="demand"):
     return bus_data
 
 
-def reset_profile_data(used_profiles, critical_time_indices, profile_types=["active", "reactive"]):
+def reset_profile_data(used_profiles, critical_time_indices, profile_types=("active", "reactive")):
     flag = dss.LoadShape.First()
     while flag > 0:
         name = dss.LoadShape.Name()
         number_of_timepoints = len(critical_time_indices)
+        # TODO: Kwami, should there be error checking on profile_types?
+        original_p_mult = None
+        original_q_mult = None
 
         if name in used_profiles:
             if "active" in profile_types:
@@ -135,13 +144,12 @@ def reset_profile_data(used_profiles, critical_time_indices, profile_types=["act
 
             dss.LoadShape.Npts(number_of_timepoints)
 
-            if len(original_p_mult) > 1:
+            if original_p_mult is not None and len(original_p_mult) > 1:
                 if len(original_p_mult) > max(critical_time_indices):
                     dss.LoadShape.PMult(list(np.array(original_p_mult)[critical_time_indices]))
                 else:
                     raise Exception("IndexError: Index out of range")
-            if len(original_q_mult) > 1:
-
+            if original_q_mult is not None and len(original_q_mult) > 1:
                 if len(original_q_mult) > max(critical_time_indices):
                     dss.LoadShape.QMult(list(np.array(original_q_mult)[critical_time_indices]))
                 else:
@@ -151,6 +159,7 @@ def reset_profile_data(used_profiles, critical_time_indices, profile_types=["act
 
 def save_circuit(output_folder):
     dss.Text.Command(f"Save Circuit Dir={output_folder}")
+    logger.info("Saved circuit to %s", output_folder)
 
 
 def get_profile_data():
@@ -169,11 +178,12 @@ def get_profile_data():
     while flag > 0:
         profile_name = dss.LoadShape.Name()
         profile_array = np.array(dss.LoadShape.PMult())
-        if not profile_name in profile_data.keys():
-            profile_data[profile_name] = {
-                "profile_name": profile_name,
-                "time_series": profile_array,
-            }
+        if profile_name in profile_data:
+            raise Exception(f"Detected duplicate profile name: {profile_name}")
+        profile_data[profile_name] = {
+            "profile_name": profile_name,
+            "time_series": profile_array,
+        }
         flag = dss.LoadShape.Next()
 
     return profile_data
@@ -194,8 +204,7 @@ def agregate_series(
 
         if dic["demand"]:
             for data in dic["demand"]:
-
-                if "demand" in ag_series[bus].keys():
+                if "demand" in ag_series[bus]:
                     ag_series[bus]["demand"] += data[0] * profile_data[data[1]]["time_series"]
                 else:
                     ag_series[bus]["demand"] = data[0] * profile_data[data[1]]["time_series"]
@@ -217,7 +226,7 @@ def agregate_series(
 
         if dic["generation"]:
             for data in dic["generation"]:
-                if "generation" in ag_series[bus].keys():
+                if "generation" in ag_series[bus]:
                     ag_series[bus]["generation"] += data[0] * profile_data[data[1]]["time_series"]
                 else:
                     ag_series[bus]["generation"] = data[0] * profile_data[data[1]]["time_series"]
@@ -228,16 +237,14 @@ def agregate_series(
                 )[0].tolist()[0]
                 ag_series[bus]["critical_time_idx"].append(max_gen_idx)
                 ag_series[bus]["condition"].append("max_generation")
-            if "demand" in ag_series[bus].keys() and "max_net_generation" in critical_conditions:
+            if "demand" in ag_series[bus] and "max_net_generation" in critical_conditions:
                 arr = ag_series[bus]["generation"] - ag_series[bus]["demand"]
                 max_netgen_idx = np.where(arr == np.amax(arr))[0].tolist()[0]
                 ag_series[bus]["critical_time_idx"].append(max_netgen_idx)
                 ag_series[bus]["condition"].append("max_net_generation")
 
-    total_gen = sum(
-        [dic["generation"] for bus, dic in ag_series.items() if "generation" in dic.keys()]
-    )
-    total_dem = sum([dic["demand"] for bus, dic in ag_series.items() if "demand" in dic.keys()])
+    total_gen = sum([dic["generation"] for bus, dic in ag_series.items() if "generation" in dic])
+    total_dem = sum([dic["demand"] for bus, dic in ag_series.items() if "demand" in dic])
     net_total_gen = total_gen - total_dem
     if "max_demand" in critical_conditions:
         max_demand_idx = np.where(total_dem == np.amax(total_dem))[0].tolist()[0]
@@ -256,25 +263,26 @@ def agregate_series(
         t
         for bus, dic in ag_series.items()
         for t in dic["critical_time_idx"]
-        if "critical_time_idx" in dic.keys()
+        if "critical_time_idx" in dic
     ]
     critical_time_indices += head_critical_time_indices
     critical_time_indices = list(set(critical_time_indices))
     critical_time_indices.sort()
     destination_model_dir = os.path.join(destination_dir, "new_model")
     os.makedirs(destination_model_dir, exist_ok=True)
+    compression_rate = 0
     if recreate_profiles:
         destination_profile_dir = os.path.join(destination_dir, "new_profiles")
         os.makedirs(destination_profile_dir, exist_ok=True)
 
-    for profile, val in profile_data.items():
-        if profile in used_profiles:
-            base_len = len(val["time_series"])
-            compression_rate = len(critical_time_indices) / base_len
-            if recreate_profiles:
-                data = val["time_series"][critical_time_indices]
-                new_profile_path = os.path.join(destination_profile_dir, f"{profile}.csv")
-                pd.DataFrame(data).to_csv(new_profile_path, index=False, header=False)
+        for profile, val in profile_data.items():
+            if profile in used_profiles:
+                base_len = len(val["time_series"])
+                compression_rate = len(critical_time_indices) / base_len
+                if recreate_profiles:
+                    data = val["time_series"][critical_time_indices]
+                    new_profile_path = os.path.join(destination_profile_dir, f"{profile}.csv")
+                    pd.DataFrame(data).to_csv(new_profile_path, index=False, header=False)
 
     reset_profile_data(used_profiles, critical_time_indices)
     save_circuit(destination_model_dir)
@@ -285,7 +293,7 @@ def agregate_series(
 def main(
     path_to_master,
     category_class_dict,
-    critical_conditions=["max_demand", "min_demand", "max_generation", "max_net_generation"],
+    critical_conditions=("max_demand", "min_demand", "max_generation", "max_net_generation"),
     recreate_profiles=False,
     destination_dir=None,
 ):
@@ -306,7 +314,7 @@ def main(
 
     """
     folder, _ = os.path.split(path_to_master)
-    if destination_dir == None:
+    if destination_dir is None:
         folder, _ = os.path.split(path_to_master)
         destination_dir = folder
     else:
