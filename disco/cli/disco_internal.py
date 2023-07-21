c4e1c14a41d25c20ac0e21e67e84d29d129dad12
Daniel Thom <daniel.thom@nrel.gov>
2023-07-21 10:31:01 -0600

    Move CLI commands from disco-internal to disco

--- disco/cli/disco_internal.py
+++ disco/cli/disco_internal.py
@@ -10,10 +10,8 @@ from disco.cli.compute_time_series_hosting_capacity import compute_time_series_h
 from disco.cli.compute_time_series_impact_analysis import compute_time_series_impact_analysis
 from disco.cli.make_cba_tables import make_cba_tables
 from disco.cli.make_upgrade_tables import make_upgrade_tables
-from disco.cli.compute_hosting_capacity import compute_hosting_capacity
 from disco.cli.cba_post_process import cba_post_process
 from disco.cli.compute_cba import compute_cba
-from disco.cli.plots import plot
 
 
 logger = logging.getLogger(__name__)
@@ -30,7 +28,5 @@ cli.add_command(compute_time_series_hosting_capacity)
 cli.add_command(compute_time_series_impact_analysis)
 cli.add_command(make_cba_tables)
 cli.add_command(make_upgrade_tables)
-cli.add_command(compute_hosting_capacity)
 cli.add_command(cba_post_process)
 cli.add_command(compute_cba)
-cli.add_command(plot)
